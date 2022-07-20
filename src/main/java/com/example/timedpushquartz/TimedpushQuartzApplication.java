21a3053bc72089ac9d8c506003da9025daa63e94
lxw <2332425872@qq.com>
2022-07-20 17:32:29 +0800

    commit

---
+++ src/main/java/com/example/timedpushquartz/TimedpushQuartzApplication.java
@@ -0,0 +1,13 @@
+package com.example.timedpushquartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimedpushQuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimedpushQuartzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/timedpushquartz/TimedpushQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timedpushquartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimedpushQuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
